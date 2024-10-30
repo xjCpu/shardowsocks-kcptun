dd6f9b85eff427acbf76db6eb89d3f3d38628930
xjCpu <154867419@qq.com>
2024-10-30 19:02:35 +0800

    Update kcptun-client.sh

--- kcptun-client.sh
+++ kcptun-client.sh
@@ -1,2 +1,2 @@
 docker pull xtaci/kcptun
-docker run -p 20001:20001 -p 20001:20001/udp -v /kcptun:/kcptun -d --restart=always  xtaci/kcptun server -c "/kcptun/config.json"
+docker run -p 49082:49082 -p 49082:49082/udp -v /kcptun:/kcptun -d --restart=always  xtaci/kcptun server -c "/kcptun/config.json"
